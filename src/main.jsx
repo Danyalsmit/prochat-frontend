c228acb1573a063485e299df582263430ae580b7
Danyal Khan <dk818552@gmail.com>
2026-06-18 23:28:55 +0500

    routing  issue

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import AppProviders from './provider/AppProviders.jsx'
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppProviders>
-      <App />
-    </AppProviders>
+    <BrowserRouter>
+      <AppProviders>
+        <App />
+      </AppProviders>
+    </BrowserRouter>
   </StrictMode>
 )
